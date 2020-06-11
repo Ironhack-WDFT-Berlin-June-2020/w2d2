283d1b9a223cf043eec2252fa10e95a6812cbab8
Jan Redmann <janredmann1@gmail.com>
2020-06-11 13:17:16 +0200

    add viking and cart solution

--- lab-ironhack-cart/starter-code/js/index.js
+++ lab-ironhack-cart/starter-code/js/index.js
@@ -2,30 +2,28 @@ var $cart = document.querySelector("#cart tbody");
 var $calc = document.getElementById("calc");
 let $create = document.querySelector("#create");
 
-$calc.onclick = calcAll;
-$create.onclick = createNewProduct;
-
 function updateSubtot(product) {
   // Iteration 1.1
-  let price = product.querySelector(".pu > span").innerHTML;
-  let quantity = product.querySelector(".qty > label > input").value;
-  let subtotal = product.querySelector(".subtot > span");
+  let priceUnitDiv = product.querySelector(".pu > span");
+  let quantityDiv = product.querySelector(".qty > label > input").value;
+  let subtotalDiv = product.querySelector(".subtot > span");
+  let priceUnit = parseInt(priceUnitDiv.innerHTML);
+  let quantity = parseInt(quantityDiv);
 
-  subtotal.innerHTML = (price * quantity);
+  subtotalDiv.innerHTML = (priceUnit * quantity).toString();
 
-  return price * quantity;
+  return priceUnit * quantity;
 }
 
 function calcAll() {
   // Iteration 1.2
   let totalPrice = 0;
-  document.querySelectorAll(".product").forEach(ele => {
+  document.querySelectorAll("#cart > tbody > tr").forEach(ele => {
     totalPrice += updateSubtot(ele);
   });
 
   document.querySelector("body > h2 > span").innerHTML = totalPrice.toString();
 }
-
 
 function addDeleteOnClick(element) {
   element.querySelector(".rm > button").onclick = deleteProduct;
@@ -35,18 +33,18 @@ function deleteProduct(event) {
   document.querySelector("#cart > tbody").removeChild(event.currentTarget.parentNode.parentNode);
 }
 
-document.querySelectorAll("#cart > tbody > tr").forEach(element => {
-  addDeleteOnClick(element);
+document.querySelectorAll("#cart > tbody > tr").forEach(ele => {
+  addDeleteOnClick(ele);
 });
 
 let oldEle = document.querySelector("#cart > tbody > tr:nth-child(1)");
-const newTrElement = oldEle.cloneNode(true); // These two lines just copies the format of the shopping cart, so i don't have to remake it
+const newEle = oldEle.cloneNode(true); // These two lines just copies the format of the shopping cart, so i don't have to remake it
 
 function createNewProduct() {
   let createDiv = document.querySelector("#cart > tfoot > tr"); // This is the "create new product div"
   let name = createDiv.querySelector("td:nth-child(1) > input").value; // This is the name input for the new product
   let priceUnit = createDiv.querySelector("td:nth-child(2) > input").value; // This is the price per unit input for the new product
-  // let newTrElement = newEle.cloneNode(true); // this create a copy of the <tr> product element
+  let newTrElement = newEle.cloneNode(true); // this create a copy of the <tr> product element
 
   newTrElement.querySelector("td.name > span").innerHTML = name; // sets the name of the new product
   newTrElement.querySelector("td.pu > span").innerHTML = priceUnit; // sets the price per unit of the new product
@@ -54,3 +52,6 @@ function createNewProduct() {
 
   document.querySelector("#cart > tbody").appendChild(newTrElement); // adds the element to the carts tbody
 }
+
+$calc.onclick = calcAll;
+$create.onclick = createNewProduct;
